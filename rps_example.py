a3325136042f9408064e36316f7314ce912b45b0
mavoo-a11y <marvinlone456@gmail.com>
2025-03-24 12:19:41 +0300

    rps_example.py

---
+++ rps_example.py
@@ -0,0 +1,11 @@
+Marvin_BMI_Weight = 45
+BMI_Weight = 20
+
+if Marvin_BMI_Weight > BMI_Weight: 
+    print("Marvin should be medium")
+elif Marvin_BMI_Weight == BMI_Weight:
+    print("medium")
+else:
+    print("Marvin should be over-weight")
+
+    
