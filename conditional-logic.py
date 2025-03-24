42ffa7eddbc25498f0cf0a6a5196dbb6b4f9cf45
mavoo-a11y <marvinlone456@gmail.com>
2025-03-24 12:21:01 +0300

    conditional-logic.py

---
+++ conditional-logic.py
@@ -0,0 +1,10 @@
+Thomas_Age =10
+Age_at_Kindergaten = 5
+# print(Thomas_Age == Age_at_Kindergaten)
+
+if Thomas_Age < Age_at_Kindergaten:
+    print("Thomas should be in pre-school")
+elif Thomas_Age == Age_at_Kindergaten:
+    print("Enjoy kindergaten")
+else:
+    print("Thomas should be in kindergaten or another class")
