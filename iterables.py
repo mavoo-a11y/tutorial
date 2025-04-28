afa906a7cf02dcf8a6af412bd8ab5a1ace9a37b9
mavoo-a11y <marvinlone456@gmail.com>
2025-04-28 20:19:50 +0300

    iterables.py

---
+++ iterables.py
@@ -0,0 +1,24 @@
+# numbers = [1,2,3,4,5]
+
+# for number in (numbers):
+#     print(number)
+
+# fruits = {"apple", "banana", 'oranges'}
+
+# for fruit in (fruits):
+#     print(fruit)
+
+# name = "Marvin Rich"
+
+# for character in name:
+#     print(character,end=" ")
+
+my_dictionary = {"A": 1, "B": 2, "C": 4, "D":5}
+
+for key, value in my_dictionary.items():
+    # print(key, value)
+    print(f"{key} = {value}")
+# else:
+#     print("No more keys left in the dictionary")
+# for value in my_dictionary.values():
+#     print(value)
